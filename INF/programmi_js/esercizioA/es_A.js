53a15bda03c6ee8bf19f0fe598f3d9c74ed286b7
shamalwe <158327803+shamalwe@users.noreply.github.com>
2024-11-20 12:56:10 +0000

    update

---
+++ INF/programmi_js/esercizioA/es_A.js
@@ -0,0 +1,11 @@
+
+    let larghezza = prompt("Inserisci la larghezza della scatola:");
+    let altezza = prompt("Inserisci l'altezza della scatola:");
+    let lunghezza = prompt("Inserisci la lunghezza della scatola:");
+
+    // Calcolo il volume
+    let volume = larghezza * altezza * lunghezza;
+
+    // Mostro il risultato nella console
+    console.log("Il volume della scatola è: " + volume);
+
